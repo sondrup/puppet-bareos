9e3b9b358a2d89d09857513bb754596650217a4e
Kim Sondrup <kim@sondrup.xyz>
2017-03-01 18:17:38 +0100

    Cleanup in bareos::ssl

--- manifests/ssl.pp
+++ manifests/ssl.pp
@@ -19,52 +19,45 @@ class bareos::ssl (
     $cafile,
   ]
 
-  File {
-    owner   => $user,
-    group   => '0',
-    mode    => '0640',
-    require => Package[$package],
-  }
+  $bareos_ssl_dir = "${conf_dir}/ssl"
 
-  file { $conf_dir:
-    ensure => 'directory',
-    owner  => $conf_user,
-    group  => $conf_group,
-  } ->
+  file {
+    default:
+      owner   => $user,
+      group   => '0',
+      mode    => '0640',
+      require => Package[$package],;
 
-  file { "${conf_dir}/ssl":
-    ensure => 'directory',
-  }
+    $conf_dir:
+      ensure => 'directory',
+      owner  => $conf_user,
+      group  => $conf_group,
+      before => File[$bareos_ssl_dir],;
 
-  file { $certfile:
-    source  => "${ssl_dir}/certs/${::clientcert}.pem",
-    require => File["${conf_dir}/ssl"],
-  }
+    $bareos_ssl_dir:
+      ensure  => 'directory',
+      require => File[$conf_dir],
+      before  => File[$ssl_files],;
 
-  file { $keyfile:
-    source  => "${ssl_dir}/private_keys/${::clientcert}.pem",
-    require => File["${conf_dir}/ssl"],
-  }
+    $certfile:
+      source  => "${ssl_dir}/certs/${::clientcert}.pem",
+      require => File[$bareos_ssl_dir],;
 
-  # Now export our key and cert files so the director can collect them,
-  # while we've still realized the actual files, except when we're on
-  # the director already.
-  #unless ($::fqdn == $bareos::params::director_name) {
-  #  @@bareos::ssl::certfile { $::clientcert: }
-  #  @@bareos::ssl::keyfile  { $::clientcert: }
-  #}
+    $keyfile:
+      source  => "${ssl_dir}/private_keys/${::clientcert}.pem",
+      require => File[$bareos_ssl_dir],;
 
-  file { $cafile:
-    ensure  => 'file',
-    source  => "${ssl_dir}/certs/ca.pem",
-    require => File["${conf_dir}/ssl"],
+    $cafile:
+      ensure  => 'file',
+      source  => "${ssl_dir}/certs/ca.pem",
+      require => File[$bareos_ssl_dir],;
   }
 
   exec { 'generate_bareos_dhkey':
     command => 'openssl dhparam -out dh1024.pem -5 1024',
     path    => '/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin:/usr/local/sbin',
-    cwd     => "${conf_dir}/ssl",
-    creates => "${conf_dir}/ssl/dh1024.pem",
-    require => File["${conf_dir}/ssl"],
+    cwd     => $bareos_ssl_dir,
+    creates => "${bareos_ssl_dir}/dh1024.pem",
+    require => File[$bareos_ssl_dir],
   }
 }
